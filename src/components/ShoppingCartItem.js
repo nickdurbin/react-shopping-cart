372a1f391432d2473cc7286923cfa6ff187d2c19
nick.durbin <nick.durbin@gmail.com>
2019-10-16 21:24:03 -0400

    Just made some minor styling changes to make all the images the same size.

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -8,7 +8,6 @@ const Item = (props) => {
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
 
-
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
